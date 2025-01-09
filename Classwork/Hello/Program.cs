81f9c03b8eb12c39e6fd9a3eb2ee3e02b1fdd151
ajay1617 <ajaybarsale5@gmail.com>
2025-01-09 12:37:13 +0530

    Asgn uploaded

---
+++ Classwork/Hello/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            //Console.Write("I Love C#");
+            //Console.WriteLine("Ajay");
+            //string str = Console.ReadLine();
+            //Console.WriteLine(str);
+            int a = 34;
+            float b = 34.4f;
+            double c = 34.4d;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+        }
+    }
+}
